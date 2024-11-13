8a2f3fdb6c8ce45e94dd2152a7b3c042772918fb
aayush maharjan <aayushmaharjan75@gmail.com>
2024-11-13 16:58:40 +0545

    aayush

--- frontend/src/Components/Layout.tsx
+++ frontend/src/Components/Layout.tsx
@@ -1,19 +1,8 @@
 import { ReactNode } from "react";
-import Header from "./Header";
-import { useSelector } from "react-redux";
-import { RootState } from "../store/store";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const status = useSelector((state: RootState) => state.auth.status);
-
   return (
     <div className="h-screen bg-primaryLightMode overflow-hidden overflow-y-auto ">
-      {status ? (
-        <div className="pt-4 ">
-          <Header />
-        </div>
-      ) : null}
-
       <main className="w-full  px-6 max-sm:px-0 ">{children}</main>
     </div>
   );
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -6,6 +6,7 @@ import Modal from "../Components/Modal";
 import axios from "axios";
 import empty from "../assets/empty.png";
 import PostModal from "../Components/PostModal";
+import Header from "../Components/Header";
 
 type PostType = {
   _id: string;
@@ -70,56 +71,61 @@ export default function Home() {
   }, []);
 
   return (
-    <Container containerStyle="bg-primaryLightMode flex-col p-10 ">
-      {showModal ? (
-        <Modal onclick={toggleModal} onPostCreated={() => fetchData()} />
-      ) : null}
-      {showPostModal ? (
-        <PostModal
-          onclick={() => togglePostModal(null, null, null)}
-          post={selectedPost}
-          onPostDeleted={handleDeletePost}
-        />
-      ) : null}
-      <div
-        className="bg-primaryGreen fixed bottom-4 right-4 cursor-pointer w-14 h-14 flex items-center justify-center rounded-full z-50"
-        onMouseEnter={() => setShowTooltip(true)}
-        onMouseLeave={() => setShowTooltip(false)}
-        onClick={() => toggleModal()}
-      >
-        <FiPlus className="text-white text-4xl font-bold" />
-        {showTooltip && (
-          <span className="absolute bottom-16 right-10 bg-gray-800 text-white text-sm py-1 px-3 rounded shadow-lg">
-            New Habit Tracker
-          </span>
-        )}
+    <>
+      <div className="my-5">
+        <Header />
       </div>
-      {posts.length === 0 ? (
-        <div className="flex justify-center items-center mt-48 flex-col">
-          <h4 className="font-medium text-2xl max-md:text-xl text-primaryGreen">
-            No Habits being tracked
-          </h4>
-          <img src={empty} alt="" width={300} />
+      <Container containerStyle="bg-primaryLightMode flex-col p-10 ">
+        {showModal ? (
+          <Modal onclick={toggleModal} onPostCreated={() => fetchData()} />
+        ) : null}
+        {showPostModal ? (
+          <PostModal
+            onclick={() => togglePostModal(null, null, null)}
+            post={selectedPost}
+            onPostDeleted={handleDeletePost}
+          />
+        ) : null}
+        <div
+          className="bg-primaryGreen fixed bottom-4 right-4 cursor-pointer w-14 h-14 flex items-center justify-center rounded-full z-50"
+          onMouseEnter={() => setShowTooltip(true)}
+          onMouseLeave={() => setShowTooltip(false)}
+          onClick={() => toggleModal()}
+        >
+          <FiPlus className="text-white text-4xl font-bold" />
+          {showTooltip && (
+            <span className="absolute bottom-16 right-10 bg-gray-800 text-white text-sm py-1 px-3 rounded shadow-lg">
+              New Habit Tracker
+            </span>
+          )}
         </div>
-      ) : (
-        <div className="grid gap-6 grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 z-10">
-          {posts.map((post, index) => (
-            <Card
-              key={index}
-              title={post.postName}
-              onClick={() => updateDateOnPost(post._id)}
-              values={post.date ? post.date : {}}
-              pannelColors={panelColors}
-              until={until}
-              isChecked={post.date ? post.date.hasOwnProperty(date) : false}
-              viewPostModal={() =>
-                togglePostModal(post.date, post.postName, post._id)
-              }
-              postId={post._id}
-            />
-          ))}
-        </div>
-      )}
-    </Container>
+        {posts.length === 0 ? (
+          <div className="flex justify-center items-center mt-48 flex-col">
+            <h4 className="font-medium text-2xl max-md:text-xl text-primaryGreen">
+              No Habits being tracked
+            </h4>
+            <img src={empty} alt="" width={300} />
+          </div>
+        ) : (
+          <div className="grid gap-6 grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 z-10">
+            {posts.map((post, index) => (
+              <Card
+                key={index}
+                title={post.postName}
+                onClick={() => updateDateOnPost(post._id)}
+                values={post.date ? post.date : {}}
+                pannelColors={panelColors}
+                until={until}
+                isChecked={post.date ? post.date.hasOwnProperty(date) : false}
+                viewPostModal={() =>
+                  togglePostModal(post.date, post.postName, post._id)
+                }
+                postId={post._id}
+              />
+            ))}
+          </div>
+        )}
+      </Container>
+    </>
   );
 }
